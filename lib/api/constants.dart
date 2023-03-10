94bab34e373c14a7a793ab2f1d69c04f10b7467e
Tolulope <tjcode19@gmail.com>
2023-03-10 14:00:05 +0100

    first commit

---
+++ lib/api/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+  static const String apiKey = '<YOUR_API_KEY_HERE>';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/original';
+}
